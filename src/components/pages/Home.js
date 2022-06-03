72f9795810dc75b054741803ecb779e79d503efc
Ifeanyi Nnorom <nnoromifeanyi@gmail.com>
2022-06-03 10:58:33 +0100

    feat: template layout

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../Layout/Layout';
+class Home extends React.Component {
+    render() {
+        return (
+            <>
+                <Layout>
+                    <h1>
+                        Home Page
+                    </h1>
+                </Layout>
+            </>
+        );
+    }
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
